a4f837816eb9cc9ce806c4cf5e2c5c710817d299
pawansonune <134114606+pawansonune@users.noreply.github.com>
2023-08-03 18:54:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var sidebar = document.querySelector(".container ");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
